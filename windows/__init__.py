23d8b1140444bd47062c979898d20e5d19b62eff
Alankarachev <90085598+Alankarachev@users.noreply.github.com>
2025-10-26 22:51:51 +0300

    __init__.py

--- windows/__init__.py
+++ windows/__init__.py
@@ -5,3 +5,9 @@ from .registration_window import RegistrationWindow
 from .partner_window import PartnerWindow # Раскомментируешь потом
 # from .warehouse_manager_window import WarehouseManagerWindow # Раскомментируешь потом
 # ... и так далее для всех окон
+
+# windows/__init__.py
+from .base_window import BaseWindow
+from .registration_window import RegistrationWindow
+
+__all__ = ['BaseWindow', 'RegistrationWindow']
